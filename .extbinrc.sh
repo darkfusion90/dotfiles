9e73644bbd253ab616c4c4415f08f38f5a8d40b7
Gaurav A. Sharma <gaurav.s@wisflux.com>
2024-06-28 21:18:32 +0530

    feat: add extbinrc

---
+++ .extbinrc.sh
@@ -0,0 +1,5 @@
+# https://github.com/nektos/act
+export PATH="${PATH:+${PATH}:}/bin/act"
+
+# https://github.com/shyiko/jabba
+[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh"
--- .mainrc.sh
+++ .mainrc.sh
@@ -1,7 +1,5 @@
 export $DOTFILES=$HOME/.customsh
 
 source $DOTFILE/.miscrc.sh
+source $DOTFILE/.extbinrc.sh
 source $DOTFILES/work/clickup/.clickuprc.sh
-
-# https://github.com/shyiko/jabba
-[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh"
